523f447c388445eeb43d9c69264cee2f1542097f
Brewy <andrea.dangelo@liceopalmeri.edu.it>
2025-04-05 12:03:48 +0200

    Update brewfetch.sh

--- brewfetch.sh
+++ brewfetch.sh
@@ -9,7 +9,6 @@ echo
 echo  "Host: $(hostname 2>/dev/null || echo "Unknown")"
 echo "Shell: $SHELL"
 echo  "OS: $(uname -o) on $(uname -m)"  
-echo  "Kernel version: $(uname -r)" 
-echo
+echo  "Kernel version: $(uname -r)"
 echo  "RAM: $(awk '/MemTotal/ {print $2 / 1024}' /proc/meminfo) MB" 
 echo  "Swap: $(awk '/SwapTotal/ {print $2 / 1024}' /proc/meminfo) MB"
